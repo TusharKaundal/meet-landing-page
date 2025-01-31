845138882c313f70a4b91ca752510c6a8ddd0eab
Tushar Kaundal <kaundaltushar@gmail.com>
2025-01-31 12:13:20 +0530

    completed the task

---
+++ script.mjs
@@ -0,0 +1,12 @@
+export function handleClick(event) {
+  if (event.target.id === "down") {
+    document.getElementById("screen_3").scrollIntoView(false);
+  }
+
+  if (event.target.id === "info") {
+    document.getElementById("screen_2").scrollIntoView(false);
+  }
+}
+document
+  .querySelectorAll("button")
+  .forEach((item) => item.addEventListener("click", handleClick));
